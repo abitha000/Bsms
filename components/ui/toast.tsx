f85d0c5c8ec05a265b418017cbb1b6851605397a
abitha000 <abithak000@yahoo.com>
2026-06-06 22:38:37 +0530

    Replace Toast components with Toaster function

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -1,16 +1,3 @@
-export type ToastActionElement = any;
-export type ToastProps = any;
-
-export const ToastProvider = ({ children }: any) => <>{children}</>
-
-export const ToastViewport = (props: any) => null
-
-export const Toast = ({ children }: any) => <>{children}</>
-
-export const ToastTitle = ({ children }: any) => <>{children}</>
-
-export const ToastDescription = ({ children }: any) => <>{children}</>
-
-export const ToastClose = (props: any) => null
-
-export const ToastAction = ({ children }: any) => <>{children}</>
+export function Toaster() {
+  return null;
+}
